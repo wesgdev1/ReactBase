2102e0b4d0d1a292ffddc481ee9f9eae2e5a9663
Welinton Suarez <wesgdeveloper@gmail.com>
2025-07-31 20:50:36 -0500

    se agrego logica en compomnente Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
 import { Card } from "./components/Card";
 
+import style from "./App.module.css";
+
 function App() {
+  const students = [
+    {
+      name: "Welinton suarez",
+      rol: "Estudiante",
+      image: "https://i.pravatar.cc/100",
+      description: "Me gusta el Frontend",
+    },
+    {
+      name: "Jhonatan Acevedo",
+      rol: "Estudiante",
+      image: "https://i.pravatar.cc/100",
+      description: "Me gusta el Backend",
+    },
+    {
+      name: "Luisa Hernadez",
+      rol: "Estudiante",
+      image: "https://i.pravatar.cc/100",
+      description: "Me gusta el Diseño",
+    },
+  ];
+
   return (
     <>
-      <Card
-        name="Welinton suarez"
-        rol="Estudiante"
-        image="https://i.pravatar.cc/100"
-      />
-      <Card
-        name="Jhonatan Acevedo"
-        rol="Estudiante"
-        image="https://i.pravatar.cc/100"
-      />
-      <Card
-        name="Luisa Hernadez"
-        rol="Estudiante"
-        image="https://i.pravatar.cc/100"
-      />
-      <Card
-        name="Welinton suarez"
-        rol="Estudiante"
-        image="https://i.pravatar.cc/100"
-      />
+      <h1 className={style.title}>Lista de estudiantes</h1>
+      <section className={style.container}>
+        {students.map((student, i) => {
+          return (
+            <Card
+              key={i}
+              name={student.name}
+              rol={student.rol}
+              image={student.image}
+              description={student.description}
+            />
+          );
+        })}
+      </section>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,12 @@
 import style from "./Card.module.css";
 
-export const Card = ({ name, rol, image }) => {
+export const Card = ({ name, rol, image, description }) => {
   return (
     <div className={style.container}>
       <img src={image} alt="photo" />
       <h2>{name}</h2>
       <strong>{rol}</strong>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam eveniet
-        et, optio nesciunt doloribus praesentium quos assumenda odio illo nisi
-        excepturi tempora omnis ipsam non laboriosam ducimus, quasi qui
-        deleniti!
-      </p>
+      <p>{description}</p>
     </div>
   );
 };
